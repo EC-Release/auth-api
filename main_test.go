c9bee57199088ce7ea2b435c451bde049474189d
EC Robot <ec.robot@ge.com>
2018-03-19 17:15:17 -0700

    add license;add test

---
+++ main_test.go
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2016 General Electric Company. All rights reserved.
+ *
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ *
+ * author: chia.chang@ge.com
+ */
+
+package main
+
+import (
+	"crypto/rsa"
+	"testing"
+
+	util "github.build.ge.com/212359746/wzutil"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+	// TODO: Add test cases.
+	}
+	for range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+func Test_decrypt(t *testing.T) {
+	type args struct {
+		d   string
+		pk  *rsa.PrivateKey
+		crt *util.Cert
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := decrypt(tt.args.d, tt.args.pk, tt.args.crt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("decrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_encrypt(t *testing.T) {
+	type args struct {
+		d   string
+		pk  *rsa.PrivateKey
+		crt *util.Cert
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := encrypt(tt.args.d, tt.args.pk, tt.args.crt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("encrypt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("encrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
